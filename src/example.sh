bf0fe92e9fcce99efc1888cb2e8dac0aed6fb3d2
jiahuei <tanjiahuei@gmail.com>
2019-06-05 18:46:07 +0800

    Update example.sh

--- src/example.sh
+++ src/example.sh
@@ -4,24 +4,32 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ${DIR}
 
 ### Training
-# Default dataset directory
+# Default
 python train.py
 
-# Custom dataset directory
+# Custom MS-COCO directory
 python train.py  \
 	--dataset_dir '/home/jiahuei/Documents/3_Datasets/MSCOCO_captions'
 
-# Word token, custom dataset directory, GPU 1
+# Word token, custom MS-COCO directory, GPU 1
 python train.py  \
 	--token_type 'word'  \
 	--dataset_dir '/home/jiahuei/Documents/3_Datasets/MSCOCO_captions'  \
 	--gpu '1'  
 
+# InstaPIC
+python train.py  \
+    --dataset_file_pattern 'insta_{}_v25595_s15'
+
+# Custom InstaPIC directory
+python train.py  \
+    --dataset_file_pattern 'insta_{}_v25595_s15'  \
+	--dataset_dir '/home/jiahuei/Documents/3_Datasets/InstaPIC'
+
 
 ### Inference
 # Default dataset and checkpoint directories
-python infer.py  \
-	--infer_checkpoints_dir 'mscoco/radix_add_softmax_h8_tie_lstm_run_01'
+python infer.py
 
 # Custom dataset and checkpoint directories
 python infer.py  \
